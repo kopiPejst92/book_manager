3414a5f5d3328e56dcb1e8123f70183fe8bf776d
kopiPejst92 <agkop01@gmail.com>
2024-10-14 13:08:07 +0200

    ngrx reducers, state, store, select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppState } from './app.state';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookReducer } from './books/book.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot<AppState>({ book: BookReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
