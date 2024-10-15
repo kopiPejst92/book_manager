068727559f24f074a92152ca02eb6dc009d0e886
kopiPejst92 <agkop01@gmail.com>
2024-10-15 15:23:45 +0200

    devtools added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppState } from './app.state';
 import { BookListComponent } from './book-list/book-list.component';
+import { BookEffects } from './books/book.effects';
 import { BookReducer } from './books/book.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,9 @@ import { BookReducer } from './books/book.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot<AppState>({ book: BookReducer })
+    StoreModule.forRoot<AppState>({ book: BookReducer }),
+    EffectsModule.forRoot([BookEffects]),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
