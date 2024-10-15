102272207ef07c378b160da47c8216242fc5099a
kopiPejst92 <agkop01@gmail.com>
2024-10-15 11:24:52 +0200

    Added onFailure and on Success actions

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,11 +1,16 @@
 import { createReducer, on } from "@ngrx/store";
 import { Book } from "../models/book";
-import { AddBook, RemoveBook } from "./book.actions";
+import { AddBook, AddBookFailure, AddBookSuccess, RemoveBook } from "./book.actions";
 
 export const initialState: Book[] = [];
 
 export const BookReducer = createReducer(
     initialState,
-    on(AddBook, (state, {id, title, author }) => [...state, { id, title, author }]),
-    on(RemoveBook, (state, {bookId}) => state.filter(book => book.id!==bookId))
+    on(AddBook, (state) => { return state }),
+    on(AddBookSuccess, (state, { id, title, author }) => [...state, { id, title, author }]),
+    on(AddBookFailure, (state, { error }) => {
+        console.error(error);
+        return state
+    }),
+    on(RemoveBook, (state, { bookId }) => state.filter(book => book.id !== bookId))
 );
